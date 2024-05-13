01014c7ef77c733c6334bddf71437f277788eede
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-05-13 17:44:14 +0300

    fix

---
+++ src/components/Loader/index.ts
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export type TProps = {};
+
+export const Loader: FC = () => null;
--- src/widgets/LoadableTariffs/index.tsx
+++ src/widgets/LoadableTariffs/index.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
-import { Loader } from 'src/components';
 import { useTariffs } from './hooks/useTariffs';
 import { TariffsPage } from '../../components/TariffsPage';
 import { Tariffs } from '../../components/Tariffs';
+import { Loader } from '../../components/Loader';
 
 export type TProps = {};
 
