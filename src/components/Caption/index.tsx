69b7a2432daeb851921a5c22b19cb7785dfbbe70
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-05-14 13:53:07 +0300

    fix

--- src/components/Caption/index.tsx
+++ src/components/Caption/index.tsx
@@ -1,6 +1,10 @@
 import React, { FC, MouseEvent } from 'react';
 import * as S from './styles';
 
+const OFFER_URL = 'https://www.invoicebox.ru/ru/offer.html?utm_source=paymentpage&utm_content=bottomlink';
+const POLICY_URL =
+    'https://www.invoicebox.ru/ru/privacypolicy/russia/main.html?utm_source=paymentpage&utm_content=bottomlink';
+
 export type TProps = {
     onLink: (event: MouseEvent<HTMLAnchorElement>) => void;
 };
@@ -8,17 +12,11 @@ export type TProps = {
 export const Caption: FC<TProps> = ({ onLink }) => (
     <S.Wrapper variant="captionRegular">
         Добавляя в заказ, вы принимаете{' '}
-        <a
-            onClick={onLink}
-            href="https://www.invoicebox.ru/ru/offer.html?utm_source=paymentpage&utm_content=bottomlink"
-        >
+        <a onClick={onLink} href={OFFER_URL}>
             пользовательское соглашение
         </a>{' '}
         и{' '}
-        <a
-            onClick={onLink}
-            href="https://www.invoicebox.ru/ru/privacypolicy/russia/main.html?utm_source=paymentpage&utm_content=bottomlink"
-        >
+        <a onClick={onLink} href={POLICY_URL}>
             правила обработки персональных данных
         </a>
     </S.Wrapper>
