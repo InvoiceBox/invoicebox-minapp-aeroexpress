b5f59f15f54b6b479688fdf5e17a49ecfa3c194d
aleradev12 <aleradev12@gmail.com>
2025-10-06 11:15:33 +0300

    chore: clear

--- src/network/http.ts
+++ src/network/http.ts
@@ -42,7 +42,7 @@ export const createOrderRequest = (data: TCreateOrder): Promise<TCreateOrderResp
         body: JSON.stringify(snakecaseKeys(data)),
     })
         .then((response) => {
-            if (!response.ok) throw new Error('Ошибка при оформлении заказа'); // *-* save (important)
+            if (!response.ok) throw new Error('Ошибка при оформлении заказа');
             return response;
         })
         .then((response) => response.json() as Promise<ICommonResponse<TCreateOrderResponse>>)
